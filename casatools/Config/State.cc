d386ef8c86a2221af3a9908d7120c50ccab0497f
Darrell Schiebel <darrell@schiebel.us>
2022-05-03 15:11:18 -0400

    change resolve( ) [for casatools only] to return a fully qualified path when the data is found directly using the search path

--- casatools/Config/State.cc
+++ casatools/Config/State.cc
@@ -1,3 +1,6 @@
+#include <numeric>
+#include <limits.h>
+#include <unistd.h>
 #include <stdexcept>
 #include <casatools/Config/State.h>
 
@@ -14,6 +17,28 @@ namespace casatools {
 
     void State::shutdown( ) {
         shutting_down = true;        
+    }
+
+    std::string State::resolve(const std::string &subdir) const {
+
+        // return immediately if subdir is null string
+        if ( subdir.size( ) <= 0 ) return subdir;
+
+        // if subdir already resolves to a valid path, try to return a
+        // fully qualified path...
+        struct stat statbuf;
+        if ( stat( subdir.c_str( ), &statbuf ) == 0 ) {
+            if ( subdir[0] == '/' ) return subdir;
+            char buf[PATH_MAX];
+            if ( getcwd(buf,sizeof(buf)) ) {
+                return std::string(buf) + "/" + subdir;
+            }
+            return subdir;
+        }
+
+        // otherwise, if data path must be searched leave it
+        // to the base class resolve method...
+        return casacore::AppState::resolve( subdir );
     }
 
 }
