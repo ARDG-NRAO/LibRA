019477f134597ddd41c372300ec7bcca8ade49e3
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2023-03-31 13:57:44 -0600

    Pushing first version of the code and build files

---
+++ apps/mssplit.cc
@@ -0,0 +1,214 @@
+#include <casa/aips.h>
+#include <ms/MSSel/MSSelection.h>
+#include <ms/MSSel/MSSelectionError.h>
+#include <ms/MSSel/MSSelectionTools.h>
+#include <ms/MSSel/MSSelectableTable.h>
+#include <tables/Tables/Table.h>
+#include <tables/Tables/PlainTable.h>
+#include <cl.h> // C++ized version
+#include <clinteract.h>
+
+using namespace std;
+using namespace casacore;
+//
+//-------------------------------------------------------------------------
+//
+#define RestartUI(Label)  {if(clIsInteractive()) {goto Label;}}
+//#define RestartUI(Label)  {if(clIsInteractive()) {clRetry();goto Label;}}
+//
+void UI(Bool restart, int argc, char **argv, string& MSNBuf, string& OutMSBuf, bool& deepCopy,
+	string& fieldStr, string& timeStr, string& spwStr, string& baselineStr,
+	string& scanStr, string& arrayStr, string& uvdistStr,string& taqlStr, string& polnStr,
+	string& stateObsModeStr, string& observationStr)
+{
+  if (!restart)
+    {
+      BeginCL(argc,argv);
+      clInteractive(0);
+    }
+  else
+   clRetry();
+  try
+    {
+      int i;
+      MSNBuf=OutMSBuf=timeStr=baselineStr=uvdistStr=scanStr=arrayStr=polnStr=stateObsModeStr="";
+      i=1;clgetSValp("ms", MSNBuf,i);  
+      i=1;clgetSValp("outms",OutMSBuf,i);  
+      i=1;clgetBValp("deepcopy",deepCopy,i);
+      clgetFullValp("field",fieldStr);
+      clgetFullValp("time",timeStr);  
+      clgetFullValp("spw",spwStr);  
+      clgetFullValp("poln",polnStr);  
+      clgetFullValp("baseline",baselineStr);  
+      clgetFullValp("scan",scanStr);  
+      clgetFullValp("array",arrayStr);  
+      clgetFullValp("uvdist",uvdistStr);  
+      clgetFullValp("stateobsmode",stateObsModeStr);  
+      clgetFullValp("observation",observationStr);  
+      dbgclgetFullValp("taql",taqlStr);  
+      EndCL();
+    }
+  catch (clError x)
+    {
+      x << x << endl;
+      clRetry();
+    }
+}
+//
+//-------------------------------------------------------------------------
+//
+void showTableCache()
+{
+  const TableCache& cache = PlainTable::tableCache();
+  Vector<String> lockedTables = cache.getTableNames();
+
+  Int n=lockedTables.nelements();
+  if(n > 0)
+    cout << endl << "####WARNING!!!!: The Table Cache has the following " << n << " entries:"  << endl;
+  
+  for (Int i=0; i<n; ++i) 
+    cout << "    " << i << ": \"" <<  lockedTables(i) << "\"" << endl;
+}
+//
+//-------------------------------------------------------------------------
+//
+void printBaselineList(Matrix<Int> list,ostream& os)
+{
+  os << "Baselines = ";
+  IPosition shp=list.shape();
+  for(Int j=0;j<shp(1);j++)
+    {
+      for(Int i=0;i<shp(0);i++)
+	os << list(i,j) << " ";
+      os << endl << "            " ;
+    }
+  os << endl;
+}
+//
+//-------------------------------------------------------------------------
+//
+void printInfo(casacore::MSSelection& msSelection)
+{
+  cout << "Ant1         = " << msSelection.getAntenna1List() << endl;
+  cout << "Ant2         = " << msSelection.getAntenna2List() << endl;
+  //  cout << "Baselines    = " << msSelection.getBaselineList() << endl;
+  cout << "Field        = " << msSelection.getFieldList()    << endl;
+  cout << "SPW          = " << msSelection.getSpwList()      << endl;
+  cout << "Chan         = " << msSelection.getChanList(NULL,1,True)     << endl;
+  cout << "Freq         = " << msSelection.getChanFreqList(NULL,True)     << endl;
+  cout << "Scan         = " << msSelection.getScanList()     << endl;
+  cout << "StateObsMode = " << msSelection.getStateObsModeList()     << endl;
+  cout << "Array        = " << msSelection.getSubArrayList() << endl;
+  cout << "Time         = " << msSelection.getTimeList()     << endl;
+  cout << "UVRange      = " << msSelection.getUVList()       << endl;
+  cout << "UV in meters = " << msSelection.getUVUnitsList()  << endl;
+  cout << "DDIDs(Poln)  = " << msSelection.getDDIDList()     << endl;
+  cout << "DDIDs(SPW)   = " << msSelection.getSPWDDIDList()     << endl;
+  cout << "PolMap       = "; for(auto m : msSelection.getPolMap()) cout << m.first << " " << m.second << endl;cout << endl;
+  cout << "CorrMap       = "; for(auto m : msSelection.getCorrMap()) cout << m.first << " " << m.second << endl;cout << endl;
+  //  cout << "CorrMap      = " << msSelection.getCorrMap( )     << endl;
+  cout << "StateList    = " << msSelection.getStateObsModeList() << endl;
+  cout << "ObservationIDList    = " << msSelection.getObservationList() << endl;
+  //  printBaselineList(msSelection.getBaselineList(),cout);
+}
+//
+//-------------------------------------------------------------------------
+//
+int main(int argc, char **argv)
+{
+  //
+  //---------------------------------------------------
+  //
+  //  MSSelection msSelection;
+  string MSNBuf,OutMSBuf,fieldStr,timeStr,spwStr,baselineStr,
+    uvdistStr,taqlStr,scanStr,arrayStr, polnStr,stateObsModeStr,
+    observationStr;
+  Bool deepCopy=0;
+  Bool restartUI=False;;
+
+ RENTER:// UI re-entry point.
+  MSNBuf=OutMSBuf=fieldStr=timeStr=spwStr=baselineStr=
+    uvdistStr=taqlStr=scanStr=arrayStr=polnStr=stateObsModeStr=observationStr="";
+  deepCopy=0;
+  fieldStr=spwStr="*";
+  fieldStr=spwStr="";
+  UI(restartUI,argc, argv, MSNBuf,OutMSBuf, deepCopy,
+     fieldStr,timeStr,spwStr,baselineStr,scanStr,arrayStr,
+     uvdistStr,taqlStr,polnStr,stateObsModeStr,observationStr);
+  restartUI = False;
+  //
+  //---------------------------------------------------
+  //
+  //      MS ms(MSNBuf,Table::Update),selectedMS(ms);
+  
+  //
+  // Make a new scope, outside of which there should be no tables left open.
+  //
+  {
+    try
+      {
+	MS ms(MSNBuf,TableLock(TableLock::AutoNoReadLocking)),selectedMS(ms);
+	//
+	// Setup the MSSelection thingi
+	//
+    
+	MSInterface msInterface(ms);
+	casacore::MSSelection msSelection;
+	casacore::MSSelectionLogError mssLEA,mssLES;
+	msSelection.setErrorHandler(casacore::MSSelection::ANTENNA_EXPR, &mssLEA);
+	msSelection.setErrorHandler(casacore::MSSelection::STATE_EXPR, &mssLES);
+
+    	// msSelection.reset(ms,MSSelection::PARSE_NOW,
+    	// 			timeStr,baselineStr,fieldStr,spwStr,
+    	// 			uvdistStr,taqlStr,polnStr,scanStr,arrayStr,
+    	// 			stateObsModeStr,observationStr);
+    	msSelection.reset(msInterface,MSSelection::PARSE_NOW,
+    			  timeStr,baselineStr,fieldStr,spwStr,
+    			  uvdistStr,taqlStr,polnStr,scanStr,arrayStr,
+    			  stateObsModeStr,observationStr);
+    	// TableExprNode ten=msSelection.toTableExprNode(&msInterface);
+    	// cerr << "TEN rows = " << ten.nrow() << endl;
+    	printInfo(msSelection);
+	
+    	if (!msSelection.getSelectedMS(selectedMS))
+    	  {
+    	    cerr << "###Informational:  Nothing selected.  ";
+    	    if (OutMSBuf != "")
+    	      cout << "New MS not written." << endl;
+    	    else
+    	      cout << endl;
+    	  }
+    	else
+    	  if (OutMSBuf != "")
+    	    if (deepCopy) selectedMS.deepCopy(OutMSBuf,Table::New);
+    	    else          selectedMS.rename(OutMSBuf,Table::New);
+    	cerr << "Number of selected rows: " << selectedMS.nrow() << endl;
+      }
+    catch (clError& x)
+      {
+    	x << x.what() << endl;
+    	restartUI=True;
+      }
+    catch (MSSelectionError& x)
+      {
+    	cerr << "###MSSelectionError: " << x.getMesg() << endl;
+    	restartUI=True;
+      }
+    //
+    // Catch any exception thrown by AIPS++ libs.  Do your cleanup here
+    // before returning to the UI (if you choose to).  Without this, all
+    // exceptions (AIPS++ or otherwise) are caught in the default
+    // exception handler (which is installed by the CLLIB as the
+    // clDefaultErrorHandler).
+    //
+    catch (AipsError& x)
+      {
+    	cerr << "###AipsError: " << x.getMesg() << endl;
+    	restartUI=True;
+      }
+  }
+
+  showTableCache();
+  //if (restartUI) 
+  restartUI=True;RestartUI(RENTER);
+}
---
+++ apps/roadrunner.cc
@@ -0,0 +1,240 @@
+//# roadrunner.cc: Driver for the AWProject class of FTMachines
+//# Copyright (C) 2021
+//# Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but WITHOUT
+//# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//# more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning AIPS++ should be addressed as follows:
+//#        Internet email: aips2-request@nrao.edu.
+//#        Postal address: AIPS++ Project Office
+//#                        National Radio Astronomy Observatory
+//#                        520 Edgemont Road
+//#                        Charlottesville, VA 22903-2475 USA
+//#
+//# $Id$
+#define ROADRUNNER_USE_HPG 1
+//#undef ROADRUNNER_USE_MPI 
+//
+// Following are from the parafeed project (the UI library)
+//
+#include <cl.h> // C++ized version
+#include <clinteract.h>
+
+#ifdef ROADRUNNER_USE_MPI
+# include <mpi.h>
+#endif
+//
+//================================================================================
+//
+
+#include <RoadRunner/rWeightor.h>
+#include <RoadRunner/DataIterations.h>
+#include <RoadRunner/DataBase.h>
+#include <RoadRunner/MakeComponents.h>
+#include <RoadRunner/Roadrunner_func.h>
+//
+//-------------------------------------------------------------------------
+//
+#define RestartUI(Label)  {if(clIsInteractive()) {goto Label;}}
+
+//#define RestartUI(Label)  {if(clIsInteractive()) {clRetry();goto Label;}}
+//
+void UI(Bool restart, int argc, char **argv, string& MSNBuf,
+	string& imageName, string& modelImageName,string& sowImageExt,
+	string& cmplxGridName, int& ImSize, int& nW,
+	float& cellSize, string& stokes, string& refFreqStr,
+	string& phaseCenter, string& weighting,
+	string& rmode, float& robust,
+	string& FTMName, string& CFCache, string& imagingMode,
+	Bool& WBAwp,string& fieldStr, string& spwStr,
+	string& uvDistStr,
+	Bool& doPointing, Bool& normalize,
+	Bool& doPBCorr,
+	Bool& conjBeams,
+	Float& pbLimit,
+	vector<float> &posigdev,
+	Bool& doSPWDataIter)
+{
+  if (!restart)
+    {
+	BeginCL(argc,argv);
+	clInteractive(0);
+      //clCmdLineFirst(0);//In an interactive session, this is supposed
+			//to keep the order of the keywords presented
+			//the same as in the code.  But it does not!
+			//The library always behaves like
+			//clCmdLineFirst(1).
+      }
+  // else
+  //   clRetry();
+ REENTER:
+      try
+    {
+      SMap watchPoints; VString exposedKeys;
+      int i;
+      MSNBuf="";
+      i=1;clgetSValp("ms", MSNBuf,i);  
+      i=1;clgetSValp("imagename", imageName,i);  
+      i=1;clgetSValp("modelimagename", modelImageName,i);  
+      i=1;clgetSValp("sowimageext", sowImageExt,i);  
+      i=1;clgetSValp("complexgrid", cmplxGridName,i);  
+
+      i=1;clgetIValp("imsize", ImSize,i);  
+      i=1;clgetFValp("cellsize", cellSize,i);  
+      i=1;clgetSValp("stokes", stokes,i);  clSetOptions("stokes",{"I","IV"});
+      i=1;clgetSValp("reffreq", refFreqStr,i);  
+      i=1;clgetSValp("phasecenter", phaseCenter,i);  
+
+      InitMap(watchPoints,exposedKeys);
+      exposedKeys.push_back("rmode");
+      exposedKeys.push_back("robust");
+      watchPoints["briggs"]=exposedKeys;
+
+      //Add watchpoints for exposing rmode and robust parameters when weight=briggs
+      i=1;clgetSValp("weighting", weighting, i ,watchPoints);
+      clSetOptions("weighting",{"natural","uniform","briggs"});
+
+      i=1;clgetSValp("rmode", rmode,i);  
+      i=1;clgetFValp("robust", robust,i);  
+      
+
+      i=1;clgetIValp("wplanes", nW,i);  
+      i=1;clgetSValp("ftm", FTMName,i); clSetOptions("ftm",{"awphpg","awproject"});
+      i=1;clgetSValp("cfcache", CFCache,i);
+      i=1;clgetSValp("mode", imagingMode,i); clSetOptions("mode",{"weight","psf","snrpsf","residual","predict"});
+      i=1;clgetBValp("wbawp", WBAwp,i); 
+      i=1;clgetSValp("field", fieldStr,i);
+      i=1;clgetSValp("spw", spwStr,i);
+      i=1;clgetSValp("uvdist", uvDistStr,i);
+      i=1;clgetBValp("pbcor", doPBCorr,i);
+      i=1;clgetBValp("conjbeams", conjBeams,i);
+      i=1;clgetFValp("pblimit", pbLimit,i);
+      InitMap(watchPoints,exposedKeys);
+      exposedKeys.push_back("pointingoffsetsigdev");
+      watchPoints["1"]=exposedKeys;
+
+
+      i=1;clgetBValp("dopointing", doPointing,i,watchPoints);
+      i=2;clgetNFValp("pointingoffsetsigdev", posigdev,i);
+
+      i=1;dbgclgetBValp("normalize",normalize,i);
+      i=1;dbgclgetBValp("spwdataiter",doSPWDataIter,i);
+     EndCL();
+
+     // do some input parameter checking now.
+     string mesgs;
+     if (phaseCenter == "")
+       mesgs += "The phasecenter parameter needs to be set. ";
+
+     if (refFreqStr == "")
+       mesgs += "The reffreq parameter needs to be set. ";
+
+     if (ImSize <= 0)
+       mesgs += "The imsize parameter needs to be set to a positive finite value. ";
+
+     if (cellSize <= 0)
+       mesgs += "The cellsize parameter needs to be set to a positive finite value. ";
+
+     if (mesgs != "")
+       clThrowUp(mesgs,"###Fatal", CL_FATAL);
+    }
+  catch (clError& x)
+    {
+      x << x << endl;
+      if (x.Severity() == CL_FATAL) exit(1);
+      //clRetry();
+      RestartUI(REENTER);
+    }
+}
+//
+//-------------------------------------------------------------------------
+//
+// Moving the embedded MPI interfaces out of the (visual) way.  If
+// necessary in the future, MPI code should be implemented in a
+// separate class and this code appropriate re-factored to be used as
+// an API for MPI class.  Ideally, MPI code should not be part of the
+// roadrunner (application layer) code.
+//#include <synthesis/TransformMachines2/test/RR_MPI.h>
+//
+
+//
+//-------------------------------------------------------------------------
+// Experimantal code not yet working
+//
+// #include <synthesis/TransformMachines2/test/ImagingEngine.h>
+//   ImagingEngine imgEngine();
+
+
+//
+//-------------------------------------------------------------------------
+//
+//
+//-------------------------------------------------------------------------
+//
+#ifdef ROADRUNNER_USE_HPG
+static const string defaultFtmName = "awphpg";
+#else // !ROADRUNNER_USE_HPG
+static const string defaultFtmName = "awproject";
+#endif // ROADRUNNER_USE_HPG
+
+int main(int argc, char **argv)
+{
+  //
+  // -------------------------------------- Just the UI -------------------------------------------------------------------
+  //
+  // The Factory Settings.
+  string MSNBuf,ftmName=defaultFtmName,
+    cfCache, fieldStr="", spwStr="*", uvDistStr="",
+    imageName, modelImageName,cmplxGridName="",phaseCenter, stokes="I",
+    refFreqStr="3.0e9", weighting="natural", sowImageExt,
+    imagingMode="residual",rmode="none";
+
+  float cellSize;//refFreq=3e09, freqBW=3e9;
+  float robust=0.0;
+  int NX=0, nW=1;//cfBufferSize=512, cfOversampling=20, nW=1;
+  bool WBAwp=true;
+  bool restartUI=false;
+  bool doPointing=false;
+  bool normalize=false;
+  bool doPBCorr= true;
+  bool conjBeams= true;
+  float pbLimit=1e-3;
+  bool doSPWDataIter=false;
+  vector<float> posigdev = {300.0,300.0};
+
+  UI(restartUI, argc, argv, MSNBuf,imageName, modelImageName,
+     sowImageExt, cmplxGridName, NX, nW, cellSize,
+     stokes, refFreqStr, phaseCenter, weighting, rmode, robust,
+     ftmName,cfCache, imagingMode, WBAwp,fieldStr,spwStr,uvDistStr,
+     doPointing,normalize,doPBCorr, conjBeams, pbLimit, posigdev,
+     doSPWDataIter);
+
+  set_terminate(NULL);
+
+  try
+    {
+      Roadrunner(restartUI, argc, argv, MSNBuf,imageName, modelImageName,
+		 sowImageExt, cmplxGridName, NX, nW, cellSize,
+		 stokes, refFreqStr, phaseCenter, weighting, rmode, robust,
+		 ftmName,cfCache, imagingMode, WBAwp,fieldStr,spwStr,uvDistStr,
+		 doPointing,normalize,doPBCorr, conjBeams, pbLimit, posigdev,
+		 doSPWDataIter);
+    }
+  catch(AipsError& er)
+    {
+      cerr << er.what() << endl;
+    }
+  return 0;
+}
