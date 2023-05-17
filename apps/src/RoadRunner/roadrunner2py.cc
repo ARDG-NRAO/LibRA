5a172a8a4040e7b66c8e64315d304e012c6d55c1
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2023-05-16 20:24:09 -0600

    Fixes in apps/src/RoadRunner/CMakeLists.txt to build the python module correctly. Commented out unncessary includes in roarunner2py.cc.
    The changes in CMakeLists.txt related to python versions don't seem relevant.
    They should'nt be (unless Py world does not honour the meaning of "major" and "minor" versions).

--- apps/src/RoadRunner/roadrunner2py.cc
+++ apps/src/RoadRunner/roadrunner2py.cc
@@ -1,14 +1,20 @@
 #include <pybind11/pybind11.h>
+#include <string>
+#include <iostream>
 #include <RoadRunner/Roadrunner_func.h>
-#include <RoadRunner/rWeightor.h>
-#include <RoadRunner/DataIterations.h>
-#include <RoadRunner/DataBase.h>
-#include <RoadRunner/MakeComponents.h>
 
 using namespace pybind11;
+using namespace std;
+
+void test(std::string& name)
+{
+	cerr << name << endl;
+}
 PYBIND11_MODULE(roadrunner2py, m) {
     m.doc() = "pybind11 roadrunner2py plugin"; // optional module docstring
 
+    
+//    m.def("test",&test, "A test function", "test"_a);
     m.def("Roadrunner_py", &Roadrunner_py, "A function which takes a string and an int and returns their sum",
           "msnbuf"_a, "imagename"_a, "modelimagename"_a, "datacolumnname"_a, "sowimageext"_a,
           "cmplxgridname"_a, "imsize"_a, "nw"_a, "cellsize"_a, "stokes"_a, "reffreqstr"_a,
