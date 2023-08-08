585a3e9b44e5cbf0778fc7ba248970dcda29f837
Mingyu Hsieh <mhsieh@dhruva.aoc.nrao.edu>
2023-08-07 23:19:47 -0600

    Removed two error handlers for two no longer needed hummbee parameters. Simplified hummbee API further based on Sanjay's cleanup on the hummbee parameters. This revision has been tested using `mtmfs` on "jet".

--- apps/src/interface/python_wrapper/hummbee2py.cc
+++ apps/src/interface/python_wrapper/hummbee2py.cc
@@ -14,31 +14,19 @@ PYBIND11_MODULE(hummbee2py, m)
 
     m.doc() = "pybind11-based hummbee python plugin"; // optional module docstring
     m.def("Hummbee", &Hummbee, "A task-level interface for the model update component",
-    "msnbuf"_a, "imagename"_a,
+    "imagename"_a,
     "modelimagename"_a="",
-    "imsize"_a,", nw"_a=1,
-    "cellsize"_a,
-    "stokes"_a="I",
-    "reffreqstr"_a="3GHz",
-    "phasecenter"_a,
-    "weighting"_a="natural",
-    "robust"_a=0.0,
-    "cfcache"_a="",
-    "fieldstr"_a="*",
-    "spwstr"_a="*",
-    "dopbcorr"_a=true,
-    "conjbeams"_a=true,
-    "pblimit"_a=0.2,
     "deconvolver"_a="hogbom",
     "scales"_a,
-    "largestscale"_a=0,
+    "largestscale"_a=-1,
     "fusedthreshold"_a=0,
-    "nterms"_a=1,
+    "nterms"_a=2,
     "gain"_a=0.1,
     "threshold"_a=0.0,
     "nsigma"_a=0.0,
-    "cycleniter"_a=1000,
-    "cyclefactor"_a=1.5,
+    "cycleniter"_a=-1,
+    "cyclefactor"_a=1.0,
     "mask"_a=""
     );
 }
+
