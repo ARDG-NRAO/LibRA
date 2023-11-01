83f3acdfbed7b58045808fdb1a92bf39494a99aa
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2023-11-01 09:55:37 -0600

    Some minor cleanup in roadrunner2py.cc

--- apps/src/RoadRunner/roadrunner2py.cc
+++ apps/src/RoadRunner/roadrunner2py.cc
@@ -8,17 +8,42 @@ using namespace std;
 
 void test(std::string& name)
 {
-	cerr << name << endl;
+  cerr << name << endl;
 }
-PYBIND11_MODULE(roadrunner2py, m) {
-    m.doc() = "pybind11 roadrunner2py plugin"; // optional module docstring
+PYBIND11_MODULE(roadrunner2py, m)
+{
+  m.doc() = "pybind11-based roadrunner python plugin"; // optional module docstring
 
-    
-//    m.def("test",&test, "A test function", "test"_a);
-    m.def("Roadrunner_py", &Roadrunner_py, "A function which takes a string and an int and returns their sum",
-          "msnbuf"_a, "imagename"_a, "modelimagename"_a, "datacolumnname"_a, "sowimageext"_a,
-          "cmplxgridname"_a, "imsize"_a, "nw"_a, "cellsize"_a, "stokes"_a, "reffreqstr"_a,
-          "phasecenter"_a, "weighting"_a, "rmode"_a, "robust"_a, "ftmname"_a, "cfcache"_a,
-          "imagingmode"_a, "wbawp"_a, "fieldstr"_a, "spwstr"_a, "uvdiststr"_a, "dopointing"_a,
-          "normalize"_a, "dopbcorr"_a, "conjbeams"_a, "pblimit"_a, "posigdev"_a, "dospwdataiter"_a);
+  //    m.def("test",&test, "A test function", "test"_a);
+
+  std::vector<float> posigdev_def(2); posigdev_def[0]=posigdev_def[1]=300.0;
+  m.def("Roadrunner", &Roadrunner, "A task-level interface for the imaging derivative component",
+	"msnbuf"_a, "imagename"_a,
+	"modelimagename"_a="",
+	"datacolumnname"_a="data",
+	"sowimageext"_a="",
+	"cmplxgridname"_a="",
+	"imsize"_a,
+	"nw"_a=1,
+	"cellsize"_a,
+	"stokes"_a="I",
+	"reffreqstr"_a="3GHz",
+	"phasecenter"_a,
+	"weighting"_a="natural",
+	"rmode"_a="",
+	"robust"_a=0.0,
+	"ftmname"_a="awprojecthpg",
+	"cfcache"_a="",
+	"imagingmode"_a="predict",
+	"wbawp"_a=true,
+	"fieldstr"_a="*",
+	"spwstr"_a="*",
+	"uvdiststr"_a="",
+	"dopointing"_a=true,
+	"normalize"_a=true,
+	"dopbcorr"_a=true,
+	"conjbeams"_a=true,
+	"pblimit"_a=0.2,
+	"posigdev"_a=posigdev_def,
+	"dospwdataiter"_a=true);
 }
