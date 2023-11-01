38d97cff594ed3e5d857747dab8fb6b622f1720a
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2023-11-01 09:55:09 -0600

    Initial commit of nearly building roadrunner2py.cc

--- apps/src/RoadRunner/roadrunner2py.cc
+++ apps/src/RoadRunner/roadrunner2py.cc
@@ -1,25 +1,19 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <string>
-#include <iostream>
 #include <RoadRunner/Roadrunner_func.h>
+#include <RoadRunner/rWeightor.h>
+#include <RoadRunner/DataIterations.h>
+#include <RoadRunner/DataBase.h>
+#include <RoadRunner/MakeComponents.h>
 
 using namespace pybind11;
-using namespace std;
-
-void test(std::string& name)
-{
-	cerr << name << endl;
-}
 PYBIND11_MODULE(roadrunner2py, m) {
     m.doc() = "pybind11 roadrunner2py plugin"; // optional module docstring
 
-    
-//    m.def("test",&test, "A test function", "test"_a);
-    m.def("Roadrunner_py", &Roadrunner_py, "A function which takes a string and an int and returns their sum",
-          "msnbuf"_a, "imagename"_a, "modelimagename"_a="", "datacolumnname"_a="data", "sowimageext"_a="",
-          "cmplxgridname"_a="", "imsize"_a, "nw"_a=1, "cellsize"_a, "stokes"_a="I", "reffreqstr"_a="3GHz",
-          "phasecenter"_a, "weighting"_a="natural", "rmode"_a="", "robust"_a=0.0, "ftmname"_a="awprojecthpg", "cfcache"_a="",
-          "imagingmode"_a="predict", "wbawp"_a=true, "fieldstr"_a="*", "spwstr"_a="*", "uvdiststr"_a="", "dopointing"_a=true,
-          "normalize"_a=true, "dopbcorr"_a=true, "conjbeams"_a=true, "pblimit"_a=0.2, "posigdev"_a={300.0,300.0}, "dospwdataiter"_a=true);
+    m.def("Roadrunner", &Roadrunner, "A function which takes a string and an int and returns their sum",
+          "restart"_a, "argc"_a, "argv"_a, 
+          "msnbuf"_a, "imagename"_a, "modelimagename"_a, "datacolumnname"_a, "sowimageext"_a,
+          "cmplxgridname"_a, "imsize"_a, "nw"_a, "cellsize"_a, "stokes"_a, "reffreqstr"_a,
+          "phasecenter"_a, "weighting"_a, "rmode"_a, "robust"_a, "ftmname"_a, "cfcache"_a,
+          "imagingmode"_a, "wbawp"_a, "fieldstr"_a, "spwstr"_a, "uvdiststr"_a, "dopointing"_a,
+          "normalize"_a, "dopbcorr"_a, "conjbeams"_a, "pblimit"_a, "posigdev"_a, "dospwdataiter"_a);
 }
