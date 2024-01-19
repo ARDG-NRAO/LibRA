830ba52b44022c01a378ea82686e7441adb9f4a5
Felipe Madsen <fmadsen@nrao.edu>
2024-01-19 11:16:43 -0700

    new function: plotConcurrencyCount to make a bar plot showing concurrent processes vs. time

--- frameworks/htclean/read_htclean_logs/jobTiming.py
+++ frameworks/htclean/read_htclean_logs/jobTiming.py
@@ -249,14 +249,20 @@ class jobTiming(object):
             # Roadrunner job - read timestamps from .out file; 1st line and line containing roadrunner::main.*done
             with open(applogfile, 'r') as log:
                 loglines = log.readlines()
-                start_time_str = getCASALogTimeStr(loglines[1])
+                for line in loglines[0:5]:
+                    if 'Opening' in line:
+                        start_time_str = getCASALogTimeStr(line)
                 for line in loglines[1:-1]:
                     if ('roadrunner::main' in line or 'roadrunner::Roadrunner_func' in line) and '...done' in line:
                         end_time_str = getCASALogTimeStr(line)
 
-        app_start_time = getTime(start_time_str)
-        app_end_time = getTime(end_time_str)
-        appExecuting = { 'start' : app_start_time, 'end' : app_end_time }
+        try:
+            app_start_time = getTime(start_time_str)
+            app_end_time = getTime(end_time_str)
+            appExecuting = { 'start' : app_start_time, 'end' : app_end_time }
+        except:
+            print('Failed to read application start/end times. Check ' + applogfile + ' for errors.')
+            appExecuting = None
         return appExecuting
 
 
@@ -275,11 +281,18 @@ class jobTiming(object):
                 for line in log.readlines():
                     if 'AWProjectFT2::init[R&D]' in line: # keyword derived from gridding duration, double check
                         start_time_str = getCASALogTimeStr(line)
-                        gridding_start_time = getTime(start_time_str)
+#                        gridding_start_time = getTime(start_time_str)
                     elif 'rows/s' in line:
                         end_time_str = getCASALogTimeStr(line)
-                        gridding_end_time = getTime(end_time_str)
-                        gridding = { 'start' : gridding_start_time, 'end' : gridding_end_time }
+#                        gridding_end_time = getTime(end_time_str)
+#                        gridding = { 'start' : gridding_start_time, 'end' : gridding_end_time }
+                try:
+                    gridding_start_time = getTime(start_time_str)
+                    gridding_end_time = getTime(end_time_str)
+                    gridding = { 'start' : gridding_start_time, 'end' : gridding_end_time }
+                except:
+                    gridding = None
+                    print('Failed to read gridding start/end times. Check ' + applogfile + ' for errors.')
         else:
             gridding = None
         return gridding
@@ -416,13 +429,20 @@ class jobTiming(object):
         """
         Converts local times from the HTCondor log to UTC
         """
-        utc = self.appExecuting['start']
-        local = self.jobExecuting['start']
-        local2UTC = int((local - utc).total_seconds() / 3600 )
-        for key in ['start', 'end']:
-            self.transferIn[key]   -= timedelta(hours = local2UTC)
-            self.transferOut[key]  -= timedelta(hours = local2UTC)
-            self.jobExecuting[key] -= timedelta(hours = local2UTC)
+        if self.appExecuting is not None:
+            utc = self.appExecuting['start']
+            local = self.jobExecuting['start']
+#            if abs((local - utc).total_seconds()) > 3600:
+            local2UTC = int((local - utc).total_seconds() / 3600 )
+            for key in ['start', 'end']:
+                self.appExecuting[key] += timedelta(hours = local2UTC)
+                if self.gridding is not None:
+                    self.gridding[key] += timedelta(hours = local2UTC)
+#                self.transferIn[key]   -= timedelta(hours = local2UTC)
+#                self.transferOut[key]  -= timedelta(hours = local2UTC)
+#                self.jobExecuting[key] -= timedelta(hours = local2UTC)
+        else:
+            print('No application time for ' + self.jobname + '. Local time not converted to UTC.')
 
 
     def getDataFrame(self):
@@ -442,8 +462,9 @@ class jobTiming(object):
         endTransferOut    = datetime.strftime(self.transferOut['end'], time_fmt)
         startJobExecuting = datetime.strftime(self.jobExecuting['start'], time_fmt)
         endJobExecuting   = datetime.strftime(self.jobExecuting['end'], time_fmt)
-        startAppExecuting = datetime.strftime(self.appExecuting['start'], time_fmt)
-        endAppExecuting   = datetime.strftime(self.appExecuting['end'], time_fmt)
+        if self.appExecuting is not None:
+            startAppExecuting = datetime.strftime(self.appExecuting['start'], time_fmt)
+            endAppExecuting   = datetime.strftime(self.appExecuting['end'], time_fmt)
         if self.gridding is not None:
             startGridding = datetime.strftime(self.gridding['start'], time_fmt)
             endGridding   = datetime.strftime(self.gridding['end'], time_fmt)
@@ -455,9 +476,13 @@ class jobTiming(object):
                 SiteName = siteName),
             dict(Task = jobname, Start = startJobExecuting, Finish = endJobExecuting, Action = 'Job executing',
                 SiteName = ''),
-            dict(Task = jobname, Start = startAppExecuting, Finish = endAppExecuting, Action = 'App executing',
-                SiteName = ''),
+#            dict(Task = jobname, Start = startAppExecuting, Finish = endAppExecuting, Action = 'App executing',
+#                SiteName = ''),
         ])
+        if 'startAppExecuting' in dir():
+            df = df.append(dict(Task = jobname,
+                Start = startAppExecuting, Finish = endAppExecuting, Action = 'App executing', SiteName = ''),
+                ignore_index = True)
         if 'startGridding' in dir():
             df = df.append(dict(Task = jobname,
                 Start = startGridding, Finish = endGridding, Action = 'Gridding', SiteName = ''),
