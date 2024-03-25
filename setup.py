d3b937f27e0e6a9f664e0ce93262f3c2d2da4a96
Preshanth Jagannathan <pjaganna@nrao.edu>
2024-03-25 03:08:52 -0600

    Commit that sets up building with scikit-build also

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+
+from skbuild import setup
+
+# Get the current working directory
+cwd = os.getcwd()
+
+# Define the install directory
+build_dir = os.path.join(cwd, '_skbuild/linux-x86_64-3.12/cmake-build/')
+install_dir = os.path.join(cwd, '_skbuild/linux-x86_64-3.12/cmake-install/libra/')
+
+
+setup(
+    name="libra",
+    version="0.1.0",
+    description="Libra is a library for imaging radio interferometry data",
+    author='Sanjay Bhatnagar, Preshanth Jagannathan, Genie Hsieh, and others',
+    license='GPL3',
+    packages=['libra'],
+    cmake_args=['-DCMAKE_BUILD_TYPE=Release',  f'-DBUILD_DIR={build_dir}', f'-DINSTALL_DIR={install_dir}'],
+    cmake_source_dir='.',
+    cmake_install_dir='.',
+    cmake_install_target='Apps'
+)
