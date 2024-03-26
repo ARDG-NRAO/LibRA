31fad7bb32a2306ef7b0ac917b8329662eb6e6c5
Preshanth Jagannathan <pjaganna@nrao.edu>
2024-03-26 17:44:35 -0600

    Setup to make a wheel for libra

--- setup.py
+++ setup.py
@@ -15,10 +15,12 @@ setup(
     version="0.1.0",
     description="Libra is a library for imaging radio interferometry data",
     author='Sanjay Bhatnagar, Preshanth Jagannathan, Genie Hsieh, and others',
+    author_email='pjaganna@nrao.edu ',
     license='GPL3',
+    include_package_data=True,
     packages=['libra'],
     cmake_args=['-DCMAKE_BUILD_TYPE=Release',  f'-DBUILD_DIR={build_dir}', f'-DINSTALL_DIR={install_dir}'],
     cmake_source_dir='.',
-    cmake_install_dir='.',
+    cmake_install_dir='_skbuild/linux-x86_64-3.12/cmake-install/libra/',
     cmake_install_target='Apps'
 )
