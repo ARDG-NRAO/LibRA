e2851b506d96d4c63c17909f2def310b9dcc4534
Felipe Madsen <fmadsen@nrao.edu>
2024-04-15 16:27:03 -0400

    Added copyright text to all files and removed unused, outdated or pycache directories

--- frameworks/htclean/hphtclean.sh
+++ frameworks/htclean/hphtclean.sh
@@ -1,9 +1,30 @@
+# Copyright (C) 2021
+# Associated Universities, Inc. Washington DC, USA.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Library General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+# License for more details.is
+#
+# You should have received a copy of the GNU Library General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+#
+# Correspondence concerning this should be addressed as follows:
+#        Postal address: National Radio Astronomy Observatory
+#                        1003 Lopezville Road,
+#                        Socorro, NM - 87801, USA
+#
+# $Id$
+
+
 #!/bin/bash
 
-# F. Madsen, 2021-08-26
-# This copy of hphtclean.sh was designed to use with /users/fmadsen/htclean/htclean
-# with option -m hpg. It calls roadrunner that runs the hpg/gpu gridder, with the specific
-# version of CASA configured below.
 
 OMP_NUM_THREADS=1
 
--- frameworks/htclean/template_CASA/stopIterations.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -f ../working/stopIMCycles ]; then
-    exit 0
-else
-    exit 1
-fi
