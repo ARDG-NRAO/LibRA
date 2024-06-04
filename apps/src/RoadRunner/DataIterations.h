ff2a36386d8482eba269ad0b570f3256cac8cbd4
Sanjay Bhatnagar <sbhatnag@nrao.edu>
2024-06-04 00:39:46 -0600

    Fixed comments and moved older commented-out code to the bottom of DataIterations.h

--- apps/src/RoadRunner/DataIterations.h
+++ apps/src/RoadRunner/DataIterations.h
@@ -86,7 +86,9 @@ public:
    * This destructor destroys the DataIterator object.
    */
   ~DataIterator() {};
-
+  //
+  //-------------------------------------------------------------------------------------------------
+  //
   /**
    * @brief Iterates over a VisibilityIterator2 object.
    *
@@ -98,10 +100,137 @@ public:
    * @param doPSF Whether to calculate the PSF.
    * @param dataCol The type of data column to use.
    * @param nVB The number of VisBuffer2 objects processed.
-   * @param imagingMode The imaging mode to use.
+   * @param dataConsumer The lambda function that consumes the VisBuffer2 produced in the iterations
    * @param cfSentNotifier A function to call when a VisBuffer2 object has been processed.
    * @return The total number of rows processed.
    */
+  std::vector<double>
+  iterVB(vi::VisibilityIterator2 *vi2,
+	 vi::VisBuffer2 *vb,
+	 int& nVB,
+	 std::function<std::vector<double> (vi::VisBuffer2* vb,vi::VisibilityIterator2 *vi2_l)> dataConsumer,
+	 std::function<void(const int&)> cfSentNotifier
+	 )
+  {
+    int vol=0,nRows=0;
+    double dataIO_time=0.0;
+    
+    std::chrono::time_point<std::chrono::steady_clock> dataIO_start;
+
+    for (vi2->origin(); vi2->more(); vi2->next())
+      {
+	auto ret=dataConsumer(vb,vi2);
+	vol += ret[0]; // Vis volume in bytes
+	dataIO_time += ret[1];
+	nRows+=vb->nRows();
+
+	cfSentNotifier(nVB);
+	
+	nVB++;
+      }
+    std::vector<double> ret={(double)vol,(double)dataIO_time,(double)nRows};
+    return ret;
+  };
+
+  //
+  //-------------------------------------------------------------------------------------------------
+  //
+  /**
+   * @brief Iterates over a VisibilityIterator2 object with chunking.
+   *
+   * This function iterates over a VisibilityIterator2 object with chunking, applying the specified FTMachine to each VisBuffer2 object and writing the results to the specified data column.
+   *
+   * @param vi2 The VisibilityIterator2 object to iterate over.
+   * @param vb2 The VisBuffer2 object to use for the iteration.
+   * @param ftm The FTMachine object to apply to each VisBuffer2 object.
+   * @param doPSF Whether to calculate the PSF.
+   * @param dataConsumer The lambda function that consumes the VisBuffer2 produced in the iterations
+   * @param waitForCFReady A function to call when a chunk has been processed.
+   * @param cfSentNotifier A function to call when a VisBuffer2 object has been processed.
+   * @return A tuple containing the total number of rows processed, the number of VisBuffer2 objects processed, and the time spent in the gridding engine.
+   */
+  std::vector<double>
+  dataIter(vi::VisibilityIterator2 *vi2,
+	   vi::VisBuffer2 *vb2,
+	   std::function<std::vector<double> (vi::VisBuffer2* vb,vi::VisibilityIterator2 *vi2_l)> dataConsumer,
+	   std::function<void(int&, int& )> waitForCFReady=[](int&, int&){},//NoOp
+	   std::function<void(const int&)> cfSentNotifier= [](const int&){} //NoOp
+	   )
+    
+  {
+    unsigned long vol=0,nRows=0;
+    int nVB=0;
+    int spwNdx=0;
+    double griddingEngine_time=0,totalDataIO_time=0.0;
+
+    ProgressMeter pm(1.0, vi2->ms().nrow(),
+		     "dataIter", "","","",true);
+
+    for (vi2->originChunks();vi2->moreChunks(); vi2->nextChunk())
+      {
+  	vi2->origin(); // So that the global vb is valid
+
+	waitForCFReady(nVB,spwNdx);
+
+  	std::chrono::time_point<std::chrono::steady_clock> griddingEngine_start
+  	  = std::chrono::steady_clock::now();
+
+	//
+	// In the following control-flow waitForCFReady() and
+	// cfSentNotifier() encapsulate coordination between the
+	// main-thread (where the gridder runs) and the cfserver-thread
+	// (which manages the CF paging).  For single-threaded operations
+	// (non HPG) these functions are a NoOp.
+	//
+	// The loop over data below blocks till waitForCFReady() returns.
+	// After CFReady signal is received from the CFServer thread, in
+	// waitForCFSent the current CFSI (cfsi_g) and the device CFArray
+	// (dcfa_sptr_g) are set for the vis resampler if a new CF set is
+	// available (due to change in VB SPW ID).  The CFArray is then
+	// sent to the device from the waitForCFReady() lambda
+	// function. iterVB() then calls cfSentNotifier() function to send
+	// the CFSent signal after successfully triggering the gridding
+	// with the current VB and CFs on the device.  CFServer in the
+	// main thread blocks till it gets the CFSent signal, after which
+	// it starts filling the DeviceCFArray with the next CF set in
+	// parallel with gridding which is always in the main thread, and
+	// issues a CFReady signal when the DeviceCFArray is ready for
+	// use.  Gridding is triggered via the call to iterVB() of this
+	// class instance, but the call is always made from the main
+	// thread.
+	//
+
+  	auto ret = iterVB(vi2, vb2,
+			  nVB,
+			  dataConsumer,
+			  cfSentNotifier);
+
+  	std::chrono::duration<double> tt = std::chrono::steady_clock::now() - griddingEngine_start;
+  	griddingEngine_time += tt.count();
+
+	vol+=ret[0];
+	totalDataIO_time+=ret[1];
+	nRows+=ret[2];
+	if (isRoot_p)
+  	  pm.update(Double(vol));
+      }
+
+    std::vector<double> ret={(double)nVB, (double)vol,(double)griddingEngine_time,(double)totalDataIO_time,(double)nRows};
+    return ret;
+  };
+
+
+private:
+  bool isRoot_p;
+  /**
+   * @brief The type of data column to use.
+   */
+  casa::refim::FTMachine::Type dataCol_l;
+};
+#endif
+
+
+
   // std::tuple<int, double>
   // iterVB(vi::VisibilityIterator2 *vi2,
   // 	     vi::VisBuffer2 *vb,
@@ -260,88 +389,3 @@ public:
   //   return std::make_tuple(nVB, vol,griddingEngine_time,totalDataIO_time);
   // };
 
-  //
-  //-------------------------------------------------------------------------------------------------
-  //
-  std::vector<double>
-  iterVB(vi::VisibilityIterator2 *vi2,
-	 vi::VisBuffer2 *vb,
-	 int& nVB,
-	 std::function<std::vector<double> (vi::VisBuffer2* vb,vi::VisibilityIterator2 *vi2_l)> dataConsumer,
-	 std::function<void(const int&)> cfSentNotifier
-	 )
-  {
-    int vol=0,nRows=0;
-    double dataIO_time=0.0;
-    
-    std::chrono::time_point<std::chrono::steady_clock> dataIO_start;
-
-    for (vi2->origin(); vi2->more(); vi2->next())
-      {
-	auto ret=dataConsumer(vb,vi2);
-	vol += ret[0]; // Vis volume in bytes
-	dataIO_time += ret[1];
-	nRows+=vb->nRows();
-
-	cfSentNotifier(nVB);
-	
-	nVB++;
-      }
-    std::vector<double> ret={(double)vol,(double)dataIO_time,(double)nRows};
-    return ret;
-  };
-
-  std::vector<double>
-  dataIter(vi::VisibilityIterator2 *vi2,
-	   vi::VisBuffer2 *vb2,
-	   std::function<std::vector<double> (vi::VisBuffer2* vb,vi::VisibilityIterator2 *vi2_l)> dataConsumer,
-	   std::function<void(int&, int& )> waitForCFReady=[](int&, int&){},//NoOp
-	   std::function<void(const int&)> cfSentNotifier= [](const int&){} //NoOp
-	   )
-    
-  {
-    unsigned long vol=0,nRows=0;
-    int nVB=0;
-    int spwNdx=0;
-    double griddingEngine_time=0,totalDataIO_time=0.0;
-
-    ProgressMeter pm(1.0, vi2->ms().nrow(),
-		     "dataIter", "","","",true);
-
-    for (vi2->originChunks();vi2->moreChunks(); vi2->nextChunk())
-      {
-  	vi2->origin(); // So that the global vb is valid
-
-	waitForCFReady(nVB,spwNdx);
-
-  	std::chrono::time_point<std::chrono::steady_clock> griddingEngine_start
-  	  = std::chrono::steady_clock::now();
-
-  	auto ret = iterVB(vi2, vb2,
-			  nVB,
-			  dataConsumer,
-			  cfSentNotifier);
-
-  	std::chrono::duration<double> tt = std::chrono::steady_clock::now() - griddingEngine_start;
-  	griddingEngine_time += tt.count();
-
-	vol+=ret[0];
-	totalDataIO_time+=ret[1];
-	nRows+=ret[2];
-	if (isRoot_p)
-  	  pm.update(Double(vol));
-      }
-
-    std::vector<double> ret={(double)nVB, (double)vol,(double)griddingEngine_time,(double)totalDataIO_time,(double)nRows};
-    return ret;
-  };
-
-
-private:
-  bool isRoot_p;
-  /**
-   * @brief The type of data column to use.
-   */
-  casa::refim::FTMachine::Type dataCol_l;
-};
-#endif
