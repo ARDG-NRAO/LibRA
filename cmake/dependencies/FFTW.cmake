a6f5a0e73c7640a06b74168648be0a97506b64ae
Preshanth Jagannathan <pjaganna@nrao.edu>
2025-08-08 00:20:05 -0600

    Updating with empty download messages for FFTW_double

--- cmake/dependencies/FFTW.cmake
+++ cmake/dependencies/FFTW.cmake
@@ -16,6 +16,7 @@ ExternalProject_Add(
 # Build double precision (default)
 ExternalProject_Add(
   FFTW_double
+  DOWNLOAD_COMMAND ""
   SOURCE_DIR      ${CMAKE_SOURCE_DIR}/dependencies/FFTW
   BINARY_DIR      ${BUILD_DIR}/FFTW_double
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -E sleep 2 && <SOURCE_DIR>/configure --enable-shared --enable-threads --enable-openmp --prefix=${INSTALL_DIR} --bindir=${INSTALL_DIR}/bin/fftw --libdir=${INSTALL_DIR}/lib
@@ -27,6 +28,7 @@ ExternalProject_Add(
 # Build single precision (float)
 ExternalProject_Add(
   FFTW_float
+  DOWNLOAD_COMMAND ""
   SOURCE_DIR      ${CMAKE_SOURCE_DIR}/dependencies/FFTW
   BINARY_DIR      ${BUILD_DIR}/FFTW_float
   CONFIGURE_COMMAND ${CMAKE_COMMAND} -E sleep 2 && <SOURCE_DIR>/configure --enable-shared --enable-threads --enable-openmp --enable-float --prefix=${INSTALL_DIR} --bindir=${INSTALL_DIR}/bin/fftw --libdir=${INSTALL_DIR}/lib
